9e22d57e50aa0802fd3f437f31356353a1dfb5bf
lishuang <lishuang@bilibili.com>
2019-07-03 10:49:15 +0800

    Initial Commit

---
+++ LuckyEgg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LuckyEgg
+//
+//  Created by 李爽 on 2019/7/3.
+//  Copyright © 2019 lishuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LuckyEgg/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LuckyEgg
+//
+//  Created by 李爽 on 2019/7/3.
+//  Copyright © 2019 lishuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
